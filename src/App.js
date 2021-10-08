7a06d87a9bcd9dcfb6c0515b12527319c949664a
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-10-08 16:59:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "assets/css/style.css";
+
+function App() {
+  return (
+    <div className='container mx-auto'>
+      <h1 className='text-6xl'>Javascript React</h1>
+    </div>
+  );
+}
+
+export default App;
