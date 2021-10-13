fc52aea63aac71c69bf1356e18feac55b68ab4e1
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-10-13 23:07:57 +0700

    update file, add file & deleted file

---
+++ src/constants/api/courses.js
@@ -0,0 +1,7 @@
+import axios from "src/configs/axios";
+
+export default {
+  all: (options = { params: { status: "published" } }) =>
+    axios.get(`/courses`, options).then((res) => res.data),
+  details: (id) => axios.get(`/courses/${id}`).then((res) => res.data),
+};
