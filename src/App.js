acea5606905ca5f6a7d50a3e325cf7f0eb8ce07d
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-10-14 22:07:33 +0700

    update file App.js & add file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 import "assets/css/style.css";
 
 import MemberRoute from "components/Routes/MemberRoute";
 import GuestRoute from "components/Routes/GuestRoute";
 
+import NotFound from "pages/404";
+import Login from "pages/Login";
+
 function App() {
+  const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
   return (
-    <div className='container mx-auto'>
-      <h1 className='text-6xl'>Javascript React</h1>
-    </div>
+    <>
+      <Router history={history}>
+        <Switch>
+          <GuestRoute path='/login' component={Login}></GuestRoute>
+
+          <Route path='*' component={NotFound}></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
